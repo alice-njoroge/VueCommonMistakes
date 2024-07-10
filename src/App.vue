33c82dde5d10becfda2fa4a5c388d6748557a5ee
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-07-10 21:00:40 +0200

    Watching Arrays - must use deep option when mutating the array and not changing it completely

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref, watch} from "vue";
+
+const numbers = ref([1, 2, 3, 4]);
+const randomNum = () => {
+  numbers.value.push(Math.floor(Math.random() * 10));
+}
+watch(
+    numbers,
+    () => console.log('New Numbers Added'), {
+      deep: true
+    })
+
+</script>
+
+<template>
+  {{numbers}}
+  <br />
+  <button @click="randomNum">
+    Push New
+  </button>
+
+</template>
+
