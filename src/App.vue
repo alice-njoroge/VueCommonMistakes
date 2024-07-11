619137c33a515acb8bb184b9a8b7f58481bb4b39
Alice Wanjiku <alicenjoroge707@gmail.com>
2024-07-11 17:31:55 +0200

    Watching Arrays - WatchEffect is deep by default

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <script setup>
-import {ref, watch} from "vue";
+import {ref, watchEffect} from "vue";
 
 const numbers = ref([1, 2, 3, 4]);
+
 const randomNum = () => {
   numbers.value.push(Math.floor(Math.random() * 10));
 }
-watch(
-    numbers,
-    () => console.log('New Numbers Added'), {
-      deep: true
-    })
-
+watchEffect(() => console.log(`New Numbers Added ${numbers.value}`),)
 </script>
 
 <template>
-  {{numbers}}
-  <br />
-  <button @click="randomNum">
-    Push New
-  </button>
+  <button @click="randomNum">Push New</button>
 
 </template>
 
